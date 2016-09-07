6f3335202ca17bff91734c1913a30bc3fa1a5866
Asmir Mustafic <goetas@gmail.com>
2016-09-07 09:13:34 +0300

    psr

--- src/Exception/ClientException.php
+++ src/Exception/ClientException.php
@@ -2,9 +2,6 @@
 
 namespace GoetasWebservices\SoapServices\SoapClient\Exception;
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-
 class ClientException extends \Exception
 {
 }
