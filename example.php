6bd36aac1ca057e6b593f40680829e517a4a57dd
Asmir Mustafic <goetas@gmail.com>
2016-09-16 17:21:53 +0300

    some examples

---
+++ example.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Example;
+
+use GlobalWeather\Container\SoapClientContainer;
+use GoetasWebservices\SoapServices\SoapClient\Builder\SoapContainerBuilder;
+use GoetasWebservices\SoapServices\SoapClient\ClientFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$container = new SoapClientContainer();
+
+$serializer = SoapContainerBuilder::createSerializerBuilderFromContainer($container)->build();
+$metadata = $container->get('goetas.soap_client.metadata_reader');
+
+$factory = new ClientFactory($metadata, $serializer);
+
+/**
+ * @var $client \GlobalWeather\SoapStubs\MortgageSoap
+ */
+$client = $factory->getClient('http://www.webservicex.net/mortgage.asmx?WSDL');
+
+$cities = $client->getMortgagePayment($years=2010, $interest=5.5, $loanAmount=100000.6, $annualTax=5, $annualInsurance=2);
+
+var_dump($cities);
+exit;
+$result = $client->getWeather("Dresden", "Germany");
+
+var_dump($result);
