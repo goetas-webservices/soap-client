4fbfb5b90cc7d8531a39721664f1582bb5a602e0
Asmir Mustafic <goetas@gmail.com>
2016-12-15 16:13:00 +0100

    tested container builder

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -2,48 +2,16 @@
 
 namespace GoetasWebservices\SoapServices\SoapClient\Tests\Client;
 
-use GoetasWebservices\SoapServices\SoapClient\ClientFactory;
-use GoetasWebservices\SoapServices\SoapClient\DependencyInjection\Compiler\CleanupPass;
-use GoetasWebservices\SoapServices\SoapClient\DependencyInjection\Configuration;
-use GoetasWebservices\SoapServices\SoapClient\DependencyInjection\SoapClientExtension;
-use GoetasWebservices\SoapServices\SoapClient\Metadata\Generator\MetadataGenerator;
-use GoetasWebservices\SoapServices\SoapClient\Metadata\Loader\DevMetadataLoader;
-use GoetasWebservices\WsdlToPhp\Tests\Generator;
-use GoetasWebservices\XML\SOAPReader\SoapReader;
-use GoetasWebservices\XML\WSDLReader\DefinitionsReader;
-use GoetasWebservices\Xsd\XsdToPhp\Naming\ShortNamingStrategy;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use GoetasWebservices\SoapServices\SoapClient\Builder\SoapContainerBuilder;
 
 class ConfigurationTest extends \PHPUnit_Framework_TestCase
 {
-
-    public function getMetadataStubs()
+    public function testDI()
     {
-        return [
-            [[]],
-            [['aa']],
-        ];
-    }
-    /**
-     * @dataProvider getMetadataStubs
-     * @param $metadata
-     */
-    public function testDI($metadata)
-    {
-        $container = new ContainerBuilder();
-        $container->registerExtension(new SoapClientExtension());
-        $container->addCompilerPass(new CleanupPass());
+        $builder = new SoapContainerBuilder(__DIR__ . '/../Fixtures/config.yml');
+        $debugContainer = $builder->getDebugContainer();
 
-        $container->setParameter('goetas_webservices.soap_client.metadata', $metadata);
-
-        $yamlLoader = new YamlFileLoader($container, new FileLocator([]));
-        $yamlLoader->load(__DIR__.'/../Fixtures/config.yml');
-
-        $container->compile();
+        $builder->dumpContainerForProd('/tmp', $debugContainer);
     }
 
 }
